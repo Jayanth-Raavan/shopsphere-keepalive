3dde44cf3eed8bb5c18097a3a860c6d092e5d31f
Jayanth <jayanthraavan@gmail.com>
2025-12-05 13:31:08 +0530

    Made it background worker

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const express = require("express");
 const axios = require("axios");
-
-const app = express();
-const port = process.env.PORT || 3000;
 
 const url = process.env.PING_URL || "https://shopsphereapi.azurewebsites.net/WeatherForecast";
 const interval = parseInt(process.env.PING_INTERVAL || "600000", 10);
@@ -18,11 +14,3 @@ async function ping() {
 
 ping();
 setInterval(ping, interval);
-
-app.get("/status", (req, res) => {
-  res.send("✅ Keep-alive pinger is running.");
-});
-
-app.listen(port, () => {
-  console.log(`Status endpoint listening on port ${port}`);
-});
